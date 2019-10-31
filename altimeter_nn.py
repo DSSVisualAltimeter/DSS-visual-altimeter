66d2ffe0bddae7f9f400294b53956f4b61b275e4
Christian Clifford <christian@cclifford.net>
2019-10-31 11:42:57 -0600

    first somewhat-working model

--- altimeter_nn.py
+++ altimeter_nn.py
@@ -3,6 +3,43 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+import os
+import random
+
+### Make sure there are trailing slashes!! ###
+data_dir = "data/"
+validation_dir = "validation/"
+
+def create_validation_data(howmany):
+  """
+  Create validation data by copying images from the data directory to the validation 
+  directory. It only copies if a random number generated between 1 and `howmany`
+  is equal to 1, so if you pass in 10, roughly 1/10 of the  images will be used for 
+  validation. 
+  THIS WILL ALL FAIL if `data_dir` and `validation_dir` do not end
+  in slashes ("/").
+  """
+  datasets = os.listdir(data_dir)
+  for s in datasets:
+    os.makedirs(validation_dir + s)
+    images = os.listdir(data_dir + s)
+    for i in images:
+      if random.randint(1, howmany) == 1:
+        os.rename(data_dir + s + "/" + i, validation_dir + s + "/" + i)
+
+def remove_validation_data():
+  """
+  Delete validation data by moving images back from the validation directory
+  to the data directory and deleting their subdirectories. 
+  """
+  validationsets = os.listdir(validation_dir)
+  for v in validationsets:
+    images = os.listdir(validation_dir + v)
+    for i in images: 
+      os.rename(validation_dir + v + "/" + i, data_dir + v + "/" +i)
+    os.rmdir(validation_dir + v)
+
+create_validation_data(10)
 
 # dimensions of our images. if your raw data is not of this size,
 # you will need to use ffmpeg (or some other tool) to figure out
@@ -11,8 +48,8 @@ img_width, img_height = 256, 256
 
 # this is where your training and testing data (images) need to be relatve to
 # where your script (altimeter_nn.py) is
-train_data_dir = '../data/train'
-validation_data_dir = '../data/validation'
+train_data_dir = data_dir
+validation_data_dir = validation_dir
 nb_train_samples = 2352
 nb_validation_samples = 1158
 epochs = 8
@@ -82,13 +119,13 @@ history = model.fit_generator(
 print(model.summary())
 
 from keras.utils import plot_model
-plot_model(model, to_file='model.png')
+plot_model(model)
 
 import matplotlib.pyplot as plt
 
 # Plot training & validation accuracy values
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
 plt.title('Model accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
@@ -107,3 +144,5 @@ plt.show(block=False)
 plt.savefig('fig2.png')
 
 model.save_weights('first_try.h5')
+
+remove_validation_data()
