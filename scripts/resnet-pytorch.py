c4e8cd619e3ba2a7cc61c31d014f4d35f73920bd
Matt Jeffs <m.s.jeffs50@live.com>
2020-03-04 19:40:43 -0700

    upload Resnet draft file. read image folder and augment data

---
+++ scripts/resnet-pytorch.py
@@ -0,0 +1,55 @@
+"""
+Purpose: Implement a pretrained model resnet50 to classify altitudes
+
+Progress: Currently this will augment and normalize data for the resnet.
+
+Code taken from Pytorch website
+
+"""
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+plt.ion()   # interactive mode
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'validation': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = '/images'
+print(data_dir)
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'validation']}
+
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'validation']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'validation']}
+class_names = image_datasets['train'].classes
+print(class_names)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
